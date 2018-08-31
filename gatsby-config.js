e2abaafb0a85933fb5860daf949e2ebb46c4292f
stormcloud266 <allprt89@gmail.com>
2018-08-31 16:03:18 -0600

    configure cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify-cms-paths`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
