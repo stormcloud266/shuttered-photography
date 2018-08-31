2981179b530e1fdb0ab6ce69fa0d2233cbeb333f
stormcloud266 <allprt89@gmail.com>
2018-08-31 15:04:56 -0600

    babel errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,9 +55,9 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-relative-images`,
-          },
+          // {
+          //   resolve: `gatsby-remark-relative-images`,
+          // },
           {
             resolve: `gatsby-remark-images`,
             options: {
