470c8e3733f1062c84475357a53015a2f7a6e945
Tawnee Allport <allprt89@gmail.com>
2020-10-01 13:16:40 -0600

    fix case

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import BlogHeader from '../components/BlogHeader'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import Footer from '../components/Footer'
 
 export default function Template({ data }) {
